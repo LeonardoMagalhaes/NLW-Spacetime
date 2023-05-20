58e20f217ca94095f52101ab5db01945e899d4d2
Leonardo Alves <Leonardo.Alves@sovos.com>
2023-05-20 17:54:54 -0300

    Aula 5 - small adjustments

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -7,6 +7,8 @@ export async function GET(request: NextRequest) {
 
   const redirectTo = request.cookies.get('redirectTo')?.value
 
+  console.log(request.cookies.get('redirectTo')?.value);
+
   const registerResponse = await api.post('/register', {
     code,
   })
